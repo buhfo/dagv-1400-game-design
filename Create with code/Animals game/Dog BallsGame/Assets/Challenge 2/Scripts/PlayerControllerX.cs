37b9dfb40e437b995fa22c80fec49836e0cfd9af
Quinn Johnston <quinnandout@gmail.com>
2024-03-07 15:59:13 -0700

    Dog Ball Challenge Fixed

---
+++ Create with code/Animals game/Dog BallsGame/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool wait = false;
+    private float spawnInterval = 0.5f;
+    void Start()
+    {
+        
+        InvokeRepeating("WaitToggle", 0, spawnInterval);
+
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (wait == false)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                wait = true;
+            }
+        }
+    }
+
+    void WaitToggle()
+    {
+        wait = false;
+    }
+}
